88b345572c82ba5c8438265ed10d3402fb73de81
Alex Wendland <me@alexwendland.com>
2015-02-25 17:19:03 -0800

    Cleanup and fix prime demo

--- demos/prime/js/main.js
+++ demos/prime/js/main.js
@@ -1,18 +1,33 @@
+var Prime = (function() {
+    var m = {};
+    
+    m.getPrimes = function(onPrime) {
+        var primeArr = [];
+        var num = 2;
+        while (primeArr.length < 100) {
+            var isPrime = true;
+            for (var t = 2; t < num; t++) {
+                if (num % t == 0) {
+                    isPrime = false;
+                    break;
+                }
+            };
+            if (isPrime) {
+                if (onPrime)
+                    onPrime(num);
+                primeArr.push(num)
+            };
+            num++;
+        }
+    };
+    
+    return m;
+})();
+
 $(function() {
-	var primeArr = [];
-	var num = 2;
-	while (primeArr.length < 100) {
-		var isPrime = true;
-		for (var t = 2; t < num; t++) {
-			if (num % t == 0) {
-				isPrime = false;
-				break;
-			}
-		};
-		if (isPrime) {
-			console.log(num);
-			primeArr.push(num)
-		};
-		num++;
-	}
+    var span = $('#prime-span');
+	Prime.getPrimes(function(prime) {
+        console.log(prime);
+        span.text(span.text() + prime + "\n");
+    });
 });
